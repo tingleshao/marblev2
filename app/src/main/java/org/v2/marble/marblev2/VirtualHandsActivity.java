ce0dd18f71de5835a3468075693bf8c498f52842
tingleshao <cshao@cs.unc.edu>
2017-09-24 00:46:16 -0700

    add new files

---
+++ app/src/main/java/org/v2/marble/marblev2/VirtualHandsActivity.java
@@ -0,0 +1,11 @@
+package org.v2.marble.marblev2;
+
+import android.app.Activity;
+
+/**
+ * Created by chongshao on 9/21/17.
+ */
+
+// TODO(chongshao): implement  SampleApplicationControl, SampleAppMenuInterface
+public class VirtualHandsActivity extends Activity {
+}
