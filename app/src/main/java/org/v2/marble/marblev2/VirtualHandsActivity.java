54e87cd6c6770e7b174c836c65df7b72f896638a
tingleshao <cshao@cs.unc.edu>
2017-09-24 00:51:16 -0700

    save update

--- app/src/main/java/org/v2/marble/marblev2/VirtualHandsActivity.java
+++ app/src/main/java/org/v2/marble/marblev2/VirtualHandsActivity.java
@@ -1,11 +1,236 @@
 package org.v2.marble.marblev2;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.vuforia.CameraDevice;
+import com.vuforia.ImageTarget;
+import com.vuforia.ObjectTracker;
+import com.vuforia.STORAGE_TYPE;
+import com.vuforia.State;
+import com.vuforia.Trackable;
+import com.vuforia.Tracker;
+import com.vuforia.TrackerManager;
 
 /**
  * Created by chongshao on 9/21/17.
  */
+// TODO(chognshao): make it marker free
+// TODO(chongshao): implement SampleAppMenuInterface
+public class VirtualHandsActivity extends Activity implements SampleApplicationControl {
+    @Override
+    public void onVuforiaUpdate(State state)
+    {
+        Log.d("DDL", "on Vuforia update called");
+    }
 
-// TODO(chongshao): implement  SampleApplicationControl, SampleAppMenuInterface
-public class VirtualHandsActivity extends Activity {
+
+    @Override
+    public boolean doDeinitTrackers()
+    {
+        // Indicate if the trackers were deinitialized correctly
+        boolean result = true;
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        tManager.deinitTracker(ObjectTracker.getClassType());
+
+        return result;
+    }
+
+
+    @Override
+    public boolean doUnloadTrackersData()
+    {
+        // Indicate if the trackers were unloaded correctly
+        boolean result = true;
+
+        // Get the image tracker:
+        TrackerManager trackerManager = TrackerManager.getInstance();
+        ObjectTracker objectTracker = (ObjectTracker) trackerManager
+                .getTracker(ObjectTracker.getClassType());
+        if (objectTracker == null)
+        {
+            Log.d(
+                    LOGTAG,
+                    "Failed to destroy the tracking data set because the ObjectTracker has not been initialized.");
+            return false;
+        }
+
+        if (dataSet != null)
+        {
+            if (!objectTracker.deactivateDataSet(dataSet))
+            {
+                Log.d(
+                        LOGTAG,
+                        "Failed to destroy the tracking data set because the data set could not be deactivated.");
+                result = false;
+            } else if (!objectTracker.destroyDataSet(dataSet))
+            {
+                Log.d(LOGTAG, "Failed to destroy the tracking data set.");
+                result = false;
+            }
+
+            if (result)
+                Log.d(LOGTAG, "Successfully destroyed the data set.");
+
+            dataSet = null;
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public boolean doInitTrackers()
+    {
+        // Indicate if the trackers were initialized correctly
+        boolean result = true;
+
+        TrackerManager tManager = TrackerManager.getInstance();
+        Tracker tracker;
+
+        tracker = tManager.initTracker(ObjectTracker.getClassType());
+        if (tracker == null)
+        {
+            Log.e(
+                    LOGTAG,
+                    "Tracker not initialized. Tracker already initialized or the camera is already started");
+            result = false;
+        } else
+        {
+            Log.i(LOGTAG, "Tracker successfully initialized");
+        }
+
+        return result;
+    }
+
+
+
+    @Override
+    public void onInitARDone(SampleApplicationException exception)
+    {
+
+        if (exception == null)
+        {
+            initApplicationAR();
+
+            mRenderer.setActive(true);
+
+            // Now add the GL surface view. It is important
+            // that the OpenGL ES surface view gets added
+            // BEFORE the camera is started and video
+            // background is configured.
+            addContentView(mGlView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+
+            // Sets the UILayout to be drawn in front of the camera
+            mUILayout.bringToFront();
+
+            // Hides the Loading Dialog
+            loadingDialogHandler
+                    .sendEmptyMessage(LoadingDialogHandler.HIDE_LOADING_DIALOG);
+
+            // Sets the layout background to transparent
+            mUILayout.setBackgroundColor(Color.TRANSPARENT);
+
+            try
+            {
+                vuforiaAppSession.startAR(CameraDevice.CAMERA_DIRECTION.CAMERA_DIRECTION_DEFAULT);
+            } catch (SampleApplicationException e)
+            {
+                Log.e(LOGTAG, e.getString());
+            }
+
+            boolean result = CameraDevice.getInstance().setFocusMode(
+                    CameraDevice.FOCUS_MODE.FOCUS_MODE_CONTINUOUSAUTO);
+
+            if (!result)
+                Log.e(LOGTAG, "Unable to enable continuous autofocus");
+
+            mSampleAppMenu = new SampleAppMenu(this, this, "Virtual Buttons",
+                    mGlView, mUILayout, null);
+            setSampleAppMenuSettings();
+
+        } else
+        {
+            Log.e(LOGTAG, exception.getString());
+            showInitializationErrorMessage(exception.getString());
+        }
+    }
+
+
+    @Override
+    public boolean doLoadTrackersData()
+    {
+        // Get the image tracker:
+        TrackerManager trackerManager = TrackerManager.getInstance();
+        ObjectTracker objectTracker = (ObjectTracker) (trackerManager
+                .getTracker(ObjectTracker.getClassType()));
+        if (objectTracker == null)
+        {
+            Log.d(
+                    LOGTAG,
+                    "Failed to load tracking data set because the ObjectTracker has not been initialized.");
+            return false;
+        }
+
+        // Create the data set:
+        dataSet = objectTracker.createDataSet();
+        if (dataSet == null)
+        {
+            Log.d(LOGTAG, "Failed to create a new tracking data.");
+            return false;
+        }
+
+        // Load the data set:
+        if (!dataSet.load("VirtualButtons/Wood.xml",
+                STORAGE_TYPE.STORAGE_APPRESOURCE))
+        {
+            Log.d(LOGTAG, "Failed to load data set.");
+            return false;
+        }
+
+        // Activate the data set:
+        if (!objectTracker.activateDataSet(dataSet))
+        {
+            Log.d(LOGTAG, "Failed to activate data set.");
+            return false;
+        }
+
+        Log.d(LOGTAG, "Successfully loaded and activated data set.");
+        return true;
+    }
+
+    @Override
+    public boolean doStartTrackers()
+    {
+        // Indicate if the trackers were started correctly
+        boolean result = true;
+
+        Tracker objectTracker = TrackerManager.getInstance().getTracker(
+                ObjectTracker.getClassType());
+        if (objectTracker != null)
+            objectTracker.start();
+
+        return result;
+    }
+
+
+    @Override
+    public boolean doStopTrackers()
+    {
+        // Indicate if the trackers were stopped correctly
+        boolean result = true;
+
+        Tracker objectTracker = TrackerManager.getInstance().getTracker(
+                ObjectTracker.getClassType());
+        if (objectTracker != null)
+            objectTracker.stop();
+
+        return result;
+    }
+
 }
+
